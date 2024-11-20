92f357398e4b3e369dd48a371d75e2d636db3309
i1011 <petercheng2000@gmail.com>
2024-11-21 01:36:03 +0800

    Add test for upsampling

--- jpeg/__main__.py
+++ jpeg/__main__.py
@@ -1,6 +1,21 @@
-from . import *
-print(dir())
-JPEG().decode(open("Image/monalisa.jpg", "rb"))
-# loads(open("Image/gig-sn01.jpg", "rb").read())
-# loads(open("Image/gig-sn08.jpg", "rb").read())
-# loads(open("Image/teatime.jpg", "rb").read())
+from jpeg import *
+from jpeg.image import np, upsample
+
+def test_upsample():
+    a = np.array([
+        [1, 2],
+        [3, 4]
+    ])
+    b = np.array([
+        [1, 1, 1, 2, 2, 2],
+        [1, 1, 1, 2, 2, 2],
+        [3, 3, 3, 4, 4, 4],
+        [3, 3, 3, 4, 4, 4],
+    ])
+    assert np.array_equal(upsample(a, 2, 3), b)
+
+test_upsample()
+files = ["monalisa", "gig-sn01", "gig-sn08", "teatime"]
+for file in files:
+    JPEG().decode(open(f"Image/{file}.jpg", "rb"))
+    break
